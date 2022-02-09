75c62fcafdaec79ca81975511413a9d0e0be4f31
roynaldi19 <roynaldi19@gmail.com>
2022-02-09 20:09:23 +0700

    update feb 22

--- src/main/kotlin/02List/List.kt
+++ src/main/kotlin/02List/List.kt
@@ -3,13 +3,15 @@ package `02List`
 fun main() {
     val list: List<String> = listOf("Roynaldi", "Kidd", "Eko")
 
-    println(list.get(0))
-    println(list.get(1))
-    println(list.get(2))
-    println(list.indexOf("Roymaldi"))
+    println(list[0])
+    println(list[1])
+    println(list[2])
+    println(list.indexOf("Roynaldi"))
     println(list.indexOf("Joko"))
     println(list.contains("Roynaldi"))
     println(list.contains("Erick"))
+    println(list.containsAll(listOf("Roynaldi", "Kidd")))
     println(list.isEmpty())
     println(list.isNotEmpty())
+
 }
--- src/main/kotlin/02List/MutableList.kt
+++ src/main/kotlin/02List/MutableList.kt
@@ -2,21 +2,16 @@ package `02List`
 
 fun main() {
     val mutableList: MutableList<String> = mutableListOf()
-
     mutableList.add("Ryo")
     mutableList.add("Kidd")
+    println(mutableList[0])
+    println(mutableList[1])
 
-    println(mutableList.get(0))
-    println(mutableList.get(1))
-    println()
-
-    mutableList.set(0,"Roynaldi")
-    println(mutableList.get(0))
-    println(mutableList.get(1))
+    mutableList[0] = "Roynaldi"
+    println(mutableList[0])
+    println(mutableList[1])
 
     mutableList.removeAt(0)
-    println()
-
     for (value in mutableList) {
         println(value)
     }
